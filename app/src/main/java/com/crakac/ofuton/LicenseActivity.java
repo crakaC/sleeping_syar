1c3663363d336b9ecc8676b372c32a5d31e0eb46
Kosuke SHIRAKASHI <k_shirakashi@acceluniverse.com>
2014-09-24 16:51:53 +0900

    optimize import

--- app/src/main/java/com/crakac/ofuton/LicenseActivity.java
+++ app/src/main/java/com/crakac/ofuton/LicenseActivity.java
@@ -1,7 +1,6 @@
 package com.crakac.ofuton;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.text.Html;
 import android.widget.TextView;
 
@@ -11,20 +10,20 @@ import java.io.InputStream;
 /**
  * Created by kosukeshirakashi on 2014/09/24.
  */
-public class LicenseActivity extends FinishableActionbarActivity{
+public class LicenseActivity extends FinishableActionbarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_license);
-        TextView textView = (TextView)findViewById(R.id.text);
-        try{
+        TextView textView = (TextView) findViewById(R.id.text);
+        try {
             InputStream is = getAssets().open("license.text");
             byte[] buf = new byte[is.available()];
             is.read(buf);
             is.close();
             String text = new String(buf);
             textView.setText(Html.fromHtml(text.replace("\n", "<br/>")));
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
