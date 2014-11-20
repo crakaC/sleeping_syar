c0fb5c35eb739a5ea3daff8dd642ee57aacc5717
Kosuke SHIRAKASHI <k_shirakashi@acceluniverse.com>
2014-11-20 18:42:06 +0900

    なんとなく例外を囲った

--- app/src/main/java/com/crakac/ofuton/util/ParallelTask.java
+++ app/src/main/java/com/crakac/ofuton/util/ParallelTask.java
@@ -4,15 +4,22 @@ import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Build;
 
+import java.util.concurrent.RejectedExecutionException;
+
 abstract public class ParallelTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
 
 	@SuppressLint("NewApi")
     @SuppressWarnings("unchecked")
-    public final AsyncTask<Params, Progress, Result> executeParallel(Params... params){
-		if(Build.VERSION.SDK_INT >= 11){
-			return super.executeOnExecutor(THREAD_POOL_EXECUTOR, params);
-		} else {
-			return super.execute(params);
-		}
+    public final void executeParallel(Params... params){
+        if(getStatus().equals(Status.RUNNING)) return;
+        try {
+            if (Build.VERSION.SDK_INT >= 11) {
+                super.executeOnExecutor(THREAD_POOL_EXECUTOR, params);
+            } else {
+                super.execute(params);
+            }
+        } catch (RejectedExecutionException e){
+            executeParallel(params);
+        }
 	}
 }
