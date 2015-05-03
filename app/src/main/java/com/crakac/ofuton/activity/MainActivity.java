40ae10201d7af6f403b5f5e62513b495b445a79f
Kosuke Shirakashi <k.shira86@gmail.com>
2015-05-03 09:14:12 +0900

    メモリ上にキープするタブ数は12

--- app/src/main/java/com/crakac/ofuton/activity/MainActivity.java
+++ app/src/main/java/com/crakac/ofuton/activity/MainActivity.java
@@ -122,9 +122,7 @@ public class MainActivity extends ActionBarActivity {
         if (savedInstanceState == null) {
             mPager.setCurrentItem(getFragmentPosition(TAB_ID_HOME));// HomeTimelineの位置に合わせる
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
-            mPager.setOffscreenPageLimit(12);// 保持するFragmentの数を指定．全フラグメントを保持するのでぬるぬる動くがメモリを食う
-        }
+        mPager.setOffscreenPageLimit(12);// 保持するFragmentの数を指定．全フラグメントを保持するのでぬるぬる動くがメモリを食う
         mTabs.setViewPager(mPager);// PagerSlidingTabStripにViewPagerをセット．
     }
 
